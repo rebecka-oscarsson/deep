cf96ee1895d14fc142eb13a7654aaf630125262f
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2023-01-06 12:50:47 +0100

    Add env variable

--- src/SocketContext.jsx
+++ src/SocketContext.jsx
@@ -1,7 +1,9 @@
 import { createContext } from "react";
 import socketIO from 'socket.io-client';
 
-const socket = socketIO.connect('http://localhost:4000');
+const backendUrl = process.env.REACT_APP_BACKEND_URL
+
+const socket = socketIO.connect(backendUrl);
 
 const SocketContext = createContext();
 
