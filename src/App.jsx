1c0114866047331b11fa2b0216445f7c543dc8c4
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2024-08-25 14:40:05 +0200

    -

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import './App.scss';
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import ChatPage from './pages/ChatPage/ChatPage';
-import {SocketProvider} from './contexts/SocketContext';
-import {UsersProvider} from './contexts/UsersContext';
+import "./App.scss";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import ChatPage from "./pages/ChatPage/ChatPage";
+import { SocketProvider } from "./contexts/SocketContext";
+import { UsersProvider } from "./contexts/UsersContext";
 
 function App() {
-
   return (
     <SocketProvider>
       <UsersProvider>
-    <HashRouter>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/talk" element={<ChatPage />}></Route>
-        </Routes>
-    </HashRouter>
-    </UsersProvider>
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/talk" element={<ChatPage />}></Route>
+          </Routes>
+        </HashRouter>
+      </UsersProvider>
     </SocketProvider>
   );
 }
